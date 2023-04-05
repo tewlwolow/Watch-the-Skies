354f7ab258eb81717a21a8bc20add6183bf23309
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-04-05 20:52:38 +0200

    UTIL: Better metadata error handling

--- 00 Core/MWSE/mods/tew/Watch the Skies/util.lua
+++ 00 Core/MWSE/mods/tew/Watch the Skies/util.lua
@@ -1,9 +1,11 @@
 local util = {}
 
 function util.metadataMissing()
+	local errorMessage = "Error! Watch the Skies-metadata.toml file is missing. Please install."
 	tes3.messageBox{
-		message = "Watch the Skies-metadata.toml file is missing. Please install."
+		message = errorMessage
 	}
+	error(errorMessage)
 end
 
 return util
